162a2eea6e8e48113bd5a5e494f40598a3d87e6e
ashish121383 <ashish121383@gmail.com>
2023-07-01 23:03:43 +0530

    fir

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # reactLearing
+
+# parcel
+- Dev Build
+- Local server
+- HMR - Hot module replacement
+- File watching Algorithm - written in c++
+- Faster build because of caching 
+- Image optimization 
+- minification file
+- Bunding ( parcel is bundler)
+- Compress 
+- consistent hashing
+- code spliting 
+- Diffenential Bunding - supprot older browsers
+- Diagonstic
+- Error handling 
+- Https 
+- Tree shaking - remove useless code
+- Different dev and prod bundles
--- practice01/app.js
+++ practice01/app.js
@@ -8,13 +8,14 @@
  *     </div>
  * </div>
  */
-
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const parent = React.createElement("div",{className:"parent"}, 
     [
-        React.createElement("div",{className:"child"}, [React.createElement("h1",{},"I am tag 1"),React.createElement("h2",{},"I am tag h2")]),
-        React.createElement("div",{className:"child1"}, [React.createElement("h1",{},"I am tag 1"),React.createElement("h2",{},"I am tag h2")])
+        React.createElement("div",{className:"child"}, [React.createElement("h1",{},"This is nameste React"),React.createElement("h2",{},"I am tag h2")]),
+        React.createElement("div",{className:"child1"}, [React.createElement("h1",{},"This is ashih barman"),React.createElement("h2",{},"I am tag h2")])
     ]);
 //object is place will give you attributes of tag
-const heading = React.createElement("h1",{id:"heading"},'I am creating my self');
+const heading = React.createElement("h1",{id:"heading"},'I am  sdasdf my self');
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
